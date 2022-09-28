892cb2628a467f477b6a3577cb4862bc36a413c8
bhargavsuri <114555466+bhargavsuri@users.noreply.github.com>
2022-09-28 10:50:53 +0530

    Add files via upload

---
+++ ex.sh
@@ -0,0 +1,14 @@
+#addition of two numbers
+echo "enter two numbers"
+read a b
+res=`expr $a + $b`
+echo "a=$a \* b=$b\t addition result=$res"
+res=`expr $a - $b`
+echo "a=$a \* b=$b\t subtraction result=$res"
+res=`expr $a \* $b`
+echo "a=$a \* b=$b\t multiplication result=$res"
+res=`expr $a % $b`
+echo "a=$a \* b=$b\t division result=$res"
+
+
+
